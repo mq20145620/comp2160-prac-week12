18ce2a9582dfce04ba08e369e46d80940546eec2
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-10-26 12:05:31 +1100

    Added Jump

--- COMP2160 Prac Week 12/Assets/Scripts/MovePlayer.cs
+++ COMP2160 Prac Week 12/Assets/Scripts/MovePlayer.cs
@@ -9,26 +9,46 @@ public class MovePlayer : MonoBehaviour
 
     public float drive = 10;
     public float drag = 1;
+    public float jumpImpulse = 50;
+
+    private Vector3 dir = Vector3.zero;
+    private bool jumpPressed = false;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 dir = Vector3.zero;
+        dir = Vector3.zero;
         dir.x = Input.GetAxis("Horizontal");
         dir.z = Input.GetAxis("Vertical");
-
         // scale so it has a maximum length of 1 in any direction
         dir = Mathf.Max(Mathf.Abs(dir.x), Mathf.Abs(dir.z)) * dir.normalized;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
         rigidbody.AddForce(drive * dir);
 
         // drag in the opposite direction to velocity
         float speed = rigidbody.velocity.magnitude;
         Vector3 dragForce = drag * speed * -rigidbody.velocity;
         rigidbody.AddForce(dragForce);
+
+        if (jumpPressed)
+        {
+            rigidbody.velocity = rigidbody.velocity + jumpImpulse * Vector3.up;
+            // only add impulse on the first frame
+            // rigidbody.AddForce(Vector3.up * jumpImpulse, ForceMode.Impulse);
+            jumpPressed = false;
+        }
+
     }
 }
